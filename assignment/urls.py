361ebbbde6dc544577493f0dfd80c2d972035a23
Ankit-7777 <ankitralawata20218@gmail.com>
2024-11-10 10:42:25 +0530

    add doc string

--- assignment/urls.py
+++ assignment/urls.py
@@ -16,3 +16,27 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+"""
+GET Request - View a Specific User's Profile by user_id
+URL: /auth/?action=manage_users&user_action=view&user_id=<user_id>
+
+GET Request - List All Users
+URL: /auth/?action=manage_users&user_action=list
+
+POST Request - Register a New User
+URL: /auth/?action=register
+
+POST Request - Login a User
+URL: /auth/?action=login
+
+POST Request - Delete a User (Superuser only)
+URL: /auth/?action=manage_users&user_action=delete&user_id=<user_id>
+
+POST Request - Update a User's Profile (Superuser only)
+URL: /auth/?action=manage_users&user_action=update&user_id=<user_id>
+
+"""
+
+
+
